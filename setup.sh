7357135381d189cbe751a8b46f7784d6a6210f26
freelo <freeloader19@gmail.com>
2023-08-07 18:14:19 +0200

    install containerd

--- setup.sh
+++ setup.sh
@@ -28,21 +28,56 @@ sudo sed -e '/swap.img/ s/^#*/#/' -i /etc/fstab
 sudo apt install docker.io -y
 
 
-# Install curl
-sudo apt install apt-transport-https curl -y
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
+EOF
+
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+sudo apt-get update
+sudo apt-get install -y containerd
+
+sudo mkdir -p /etc/containerd
+sudo containerd config default | sudo tee /etc/containerd/config.toml
+sudo vi /etc/containerd/config.toml
+#find the [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.runc.options] section and change systemdcgroup to true
+#SystemdCgroup = true
+
+sudo systemctl restart containerd
+sudo systemctl enable containerd
+sudo containerd config dump
+
+# sysctl params required by setup, params persist across reboots
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward                 = 1
+EOF
+
+# Apply sysctl params without reboot
+sudo sysctl --system
+
+sysctl net.bridge.bridge-nf-call-iptables net.bridge.bridge-nf-call-ip6tables net.ipv4.ip_forward
+
+
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl
 
 # Add repositoy key for downloading and installing kubernetes
-curl -fsSL https://dl.k8s.io/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
+curl -fsSL https://dl.k8s.io/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpgecho "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
-sudo apt update
+sudo apt-get update
 
 # Install kubernetes apps
 
-sudo apt-get install kubeadm kubelet kubectl kubernetes-cni -y
+sudo apt-get install -y kubelet kubeadm kubectl kubernetes-cni
 
 
 if [[ "${k8_nodetype^}" = "master" ]]; then
     echo 'Init Cluster'
-    sudo kubeadm init
+#    sudo kubeadm init
 fi
