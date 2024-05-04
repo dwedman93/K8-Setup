5f2e3a32c7ab88948ababf849aaa50277b760296
dwedman93 <141575834+dwedman93@users.noreply.github.com>
2024-05-04 14:52:04 +0200

    Update master.sh

--- master.sh
+++ master.sh
@@ -29,6 +29,3 @@ sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
 # Install CNI Plugin (Calico) Others could be flannel, cilium, etc.
 sudo kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
-
-#kubeadm join 192.168.178.88:6443 --token 5zinav.y109w0oxubbq2ar1 --discovery-token-ca-cert-hash sha256:747a0516a97606878e156b0c47347a32d6b505beab66801b83d1c669e9c8d3a3
-#sudo kubeadm join 192.168.178.51:6443 --token trg25f.7s8c3u2xz9uguzel   --discovery-token-ca-cert-hash sha256:519f38826fd3c12679736a206fd4bd52f5b148dd82ec981fb1dfb5a7733577c0
